d3e846f03702bde131b700e533a08a2f3213671d
skyzha053 <skyzha107@outlook.com>
2023-02-16 18:48:39 +0100

    cocktails to discover gemaakt

--- src/pages/mainmenu/mainmenu.js
+++ src/pages/mainmenu/mainmenu.js
@@ -5,11 +5,12 @@ import Footerlogin from "../../components/Footerlogin/Footerlogin";
 import { Link } from 'react-router-dom'
 
 function MainMenu() {
-    const [imageURL, setImageURL] = useState('');
-    const [cocktailId, setCocktailId] = useState('');
+    const [imageURLs, setImageURLs] = useState([]);
+    const [cocktailId, setCocktails] = useState([]);
+    const [cocktailNames, setCocktailNames] = useState([]);
 
-    const handleClick = () => {
-        axios.get(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${cocktailId}`)
+    const handleClick = (id) => {
+        axios.get(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`)
             .then(response => {
                 const cocktail = response.data.drinks[0];
                 const cocktailDetails = {
@@ -35,30 +36,35 @@ function MainMenu() {
                     queryParams.append(`ingredient${index + 1}`, `${ingredient.ingredient} - ${ingredient.measure}`);
                 });
 
-                window.location.href = `/recept?${queryParams.toString()}`;
+                window.location.href = `/recept?id=${id}&${queryParams.toString()}`;
             })
             .catch(error => {
                 console.log(error);
             });
     }
 
-    const getRandomCocktailImage = () => {
-        axios.get('https://www.thecocktaildb.com/api/json/v1/1/random.php')
-            .then(response => {
-                const cocktail = response.data.drinks[0];
-                const image = cocktail.strDrinkThumb;
-                const id = cocktail.idDrink;
-                setImageURL(image);
-                setCocktailId(id);
-            })
-            .catch(error => {
-                console.log(error);
-            });
+    const getRandomCocktails = () => {
+        Promise.all([
+            axios.get('https://www.thecocktaildb.com/api/json/v1/1/random.php'),
+            axios.get('https://www.thecocktaildb.com/api/json/v1/1/random.php'),
+            axios.get('https://www.thecocktaildb.com/api/json/v1/1/random.php'),
+            axios.get('https://www.thecocktaildb.com/api/json/v1/1/random.php'),
+            axios.get('https://www.thecocktaildb.com/api/json/v1/1/random.php')
+        ]).then(responses => {
+            const cocktails = responses.map(response => response.data.drinks[0]);
+            setImageURLs(cocktails.map(cocktail => cocktail.strDrinkThumb));
+            setCocktails(cocktails.map(cocktail => cocktail.idDrink));
+            setCocktailNames(cocktails.map(cocktail => cocktail.strDrink))
+        }).catch(error => {
+            console.log(error);
+        });
     }
+
 
     useEffect(() => {
-        getRandomCocktailImage();
+        getRandomCocktails();
     }, []);
+
 
     return (
         <div className="mainmenu">
@@ -66,9 +72,44 @@ function MainMenu() {
                 <h1>Cocktails to discover</h1>
             </div>
             <div className="images-cocktails">
-                <Link to={`/recept?id=${cocktailId}`}>
-                    <img src={imageURL} alt="Cocktail" onClick={handleClick} />
+                <Link to={`/recept?id=${cocktailId[0]}`}>
+                    <img src={imageURLs[0]} alt="Cocktail" onClick={() => handleClick(cocktailId[0])} />
                 </Link>
+                <div className="cocktail-name">
+                    <p>{cocktailNames[0]}</p>
+                </div>
+            </div>
+            <div className="images-cocktails">
+                <Link to={`/recept?id=${cocktailId[1]}`}>
+                    <img src={imageURLs[1]} alt="Cocktail" onClick={() => handleClick(cocktailId[1])} />
+                </Link>
+                <div className="cocktail-name">
+                    <p>{cocktailNames[1]}</p>
+                </div>
+            </div>
+            <div className="images-cocktails">
+                <Link to={`/recept?id=${cocktailId[2]}`}>
+                    <img src={imageURLs[2]} alt="Cocktail" onClick={() => handleClick(cocktailId[2])} />
+                </Link>
+                <div className="cocktail-name">
+                    <p>{cocktailNames[2]}</p>
+                </div>
+            </div>
+            <div className="images-cocktails">
+                <Link to={`/recept?id=${cocktailId[3]}`}>
+                    <img src={imageURLs[3]} alt="Cocktail" onClick={() => handleClick(cocktailId[3])} />
+                </Link>
+                <div className="cocktail-name">
+                    <p>{cocktailNames[3]}</p>
+                </div>
+            </div>
+            <div className="images-cocktails">
+                <Link to={`/recept?id=${cocktailId[4]}`}>
+                    <img src={imageURLs[4]} alt="Cocktail" onClick={() => handleClick(cocktailId[4])} />
+                </Link>
+                <div className="cocktail-name">
+                    <p>{cocktailNames[4]}</p>
+                </div>
             </div>
             <Footerlogin />
         </div>
