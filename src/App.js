88b18b349019fc5040573e00c1a8b125eca43030
skyzha053 <skyzha107@outlook.com>
2023-03-01 20:39:19 +0100

    paginas beveiligde route gegeven

--- src/App.js
+++ src/App.js
@@ -1,31 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css'
-import {Routes, Route } from 'react-router-dom'
-import loginPage from './pages/login/login'
+import { Routes, Route, Navigate } from 'react-router-dom'
+import LoginPage from './pages/login/login'
 import Navbar from "./components/Navbar/Navbar";
 import FooterLogin from "./components/Footerlogin/Footerlogin";
-import forgetPassword from './pages/forget/forget'
-import registerForm from './pages/register/register'
+import ForgetPassword from './pages/forget/forget'
+import RegisterForm from './pages/register/register'
 import MainMenu from "./pages/mainmenu/mainmenu";
 import Recept from "./pages/recept/recept";
 
-
 function App() {
-  return (
+    const [isAuthenticated, setIsAuthenticated] = useState(false);
 
-    <div>
-      <Navbar />
-        <Routes>
-            <Route path="/" element={loginPage()}/>
-            <Route path="/forget" element={forgetPassword()}/>
-            <Route path="/register" element={registerForm()}/>
-            <Route path="/main-menu" element={MainMenu()}/>
-            <Route path="/recept" element={Recept()}/>
-        </Routes>
+    const handleAuthentication = () => {
+        setIsAuthenticated(true);
+    };
 
+    const LoginRequiredPage = () => {
+        return (
+            <div>
+                <p>You need to log in to access this page.</p>
+                <Navigate to="/" />
+            </div>
+        );
+    };
 
-    </div>
-  );
+    return (
+        <div>
+            <Navbar />
+            <Routes>
+                <Route
+                    path="/"
+                    element={<LoginPage onLogin={handleAuthentication} />}
+                />
+                <Route path="/forget" element={<ForgetPassword />} />
+                <Route path="/register" element={<RegisterForm />} />
+                {isAuthenticated ? (
+                    <>
+                        <Route path="/main-menu" element={<MainMenu />} />
+                        <Route path="/recept" element={<Recept />} />
+                    </>
+                ) : (
+                    <>
+                        <Route path="/main-menu" element={<LoginRequiredPage />} />
+                        <Route path="/recept" element={<LoginRequiredPage />} />
+                    </>
+                )}
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
